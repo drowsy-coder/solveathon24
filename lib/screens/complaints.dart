01c6f76ad3f80be43b2f0a500a4406f2a4a67bdc
Amritansh Sharma <amritansh.code@gmail.com>
2024-04-09 20:53:50 +0530

    .

--- lib/screens/complaints.dart
+++ lib/screens/complaints.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:dropdown_textfield/dropdown_textfield.dart';
 
 class ComplaintFormScreen extends StatelessWidget {
